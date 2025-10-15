e5923a60c40113a49913977107cedb2dcf93aa1e
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-16 00:42:34 +0900

    feat: 커스텀 구분자로 파싱한다.

--- src/main/kotlin/calculator/domain/CustomParser.kt
+++ src/main/kotlin/calculator/domain/CustomParser.kt
@@ -7,4 +7,11 @@ class CustomParser : Parser{
     }
 
     fun recognizeSeparator(command: String): String = command.substringAfter("//").substringBefore("\\n")
+
+    fun parseByCustomSeparator(separator: String, command: String): List<Int> {
+        val numbersString = command.substringAfter("\\n")
+        return numbersString.split(separator)
+            .map { it.trim() }
+            .map { it.toInt() }
+    }
 }
