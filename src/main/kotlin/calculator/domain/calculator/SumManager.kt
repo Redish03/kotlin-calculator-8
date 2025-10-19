939ff04781fbb5c599f6c11f06d322a3a1a221d3
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-19 14:22:12 +0900

    test: 숫자 리스트의 원소들을 모두 합하는 테스트 작성

---
+++ src/main/kotlin/calculator/domain/calculator/SumManager.kt
@@ -0,0 +1,4 @@
+package calculator.domain.calculator
+
+class SumManager {
+}
---
+++ src/test/kotlin/calculator/domain/calculator/SumManagerTest.kt
@@ -0,0 +1,19 @@
+package calculator.domain.calculator
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SumManagerTest {
+    @Test
+    fun `숫자 리스트의 원소들을 더한다`() {
+        // given:
+        val numbers = listOf(1, 2, 5, 6)
+        val expectedAnswer = 14
+
+        // when:
+        val calculator = SumManager.addAll(numbers)
+
+        // then:
+        assertEquals(calculator, expectedAnswer)
+    }
+}
