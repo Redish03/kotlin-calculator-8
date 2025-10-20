21e251c30a921e3b93aacf8f9ac894beb3790bf8
SeongWooHong <pluto0303@naver.com>
2025-10-20 18:16:06 +0900

    feat: 파서 분류 로직 추가

--- src/main/kotlin/calculator/Controller.kt
+++ src/main/kotlin/calculator/Controller.kt
@@ -1,6 +1,7 @@
 package calculator
 
 import calculator.domain.Parser
+import calculator.domain.ParserClassifier
 import calculator.domain.calculator.SumManager
 import calculator.view.InputManager
 import calculator.view.OutputManager
@@ -14,8 +15,8 @@ class Controller {
         outputManager.printGuide()
         val command = inputManager.inputCommandFromUser()
         try {
-            isSatisfyCondition(command)
             val numbers = parseCommand(command)
+            isSatisfyCondition(command)
             OutputManager().printResult(sumManager.addAll(numbers))
         } catch (e: IllegalArgumentException) {
 
@@ -27,6 +28,7 @@ class Controller {
     }
 
     fun parseCommand(command: String) : List<Int> {
-
+        val parser: Parser = parserClassifier.create(command)
+        return parser.parseBySeparator(command)
     }
 }
